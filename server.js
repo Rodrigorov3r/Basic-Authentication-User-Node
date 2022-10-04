95adf81785c57ea019c146e4fc25fd9b6345c9b3
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-10-04 10:58:24 -0300

    post user

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-import Express, { json } from 'express';
+import express, { json } from 'express';
 import Bcrypt from 'bcrypt';
 
-const app = new Express();
+const app = express();
 
-app.use(json());
+app.use(express.json());
 app.listen(3001, () => console.log('server ok!'));
 
 const users = []; //a modo de prueba creo un obj y lo uso de acceso directo
 
-app.get('/', (req, res) => res.json(users));
+app.get('/users', (req, res) => res.json(users));
+
+app.post('/users', (req, res) => {
+  const user = {
+    name: req.body.name,
+    pass: req.body.pass,
+  };
+  users.push(user)
+  res.status(201).send({message: "post ok"})
+});
